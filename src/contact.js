777626528d09c8df4e9266794fd6434f358f3a5d
David Tran <davethedeveloper6@gmail.com>
2024-10-17 20:51:34 -0700

    saving cause problems

---
+++ src/contact.js
@@ -0,0 +1,13 @@
+//create the function 
+//keep it simple
+
+function createContactPage() {
+    const content = document.querySelector('content');
+    content.tectContent = '';
+
+    //main backcard---
+    
+
+}
+
+export default createContactPage;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './styles.css';
 import createHomePage from './home.js';
 import createMenuPage from './menu.js';
-// import createContactPage from './contact.js';
+import createContactPage from './contact.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     //Load the Home page initially
